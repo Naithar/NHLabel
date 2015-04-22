84eb1d1762cf8c1be40acd53cb95795d3a0f4d41
Naithar <devias.naith@gmail.com>
2015-04-22 19:46:49 +0300

    more code for text bounds.

--- Pod/Classes/NLabel.m
+++ Pod/Classes/NLabel.m
@@ -35,6 +35,44 @@
 //    }
 
 
+//- (CGRect)textRectForBounds:(CGRect)bounds
+//limitedToNumberOfLines:(NSInteger)numberOfLines
+//{
+//    bounds = UIEdgeInsetsInsetRect(bounds, self.textInsets);
+//    if (!self.attributedText) {
+//        return [super textRectForBounds:bounds limitedToNumberOfLines:numberOfLines];
+//    }
+//
+//    CGRect textRect = bounds;
+//
+//    // Calculate height with a minimum of double the font pointSize, to ensure that CTFramesetterSuggestFrameSizeWithConstraints doesn't return CGSizeZero, as it would if textRect height is insufficient.
+//    textRect.size.height = MAX(self.font.lineHeight * MAX(2, numberOfLines), bounds.size.height);
+//
+//    // Adjust the text to be in the center vertically, if the text size is smaller than bounds
+//    CGSize textSize = CTFramesetterSuggestFrameSizeWithConstraints([self framesetter], CFRangeMake(0, (CFIndex)[self.attributedText length]), NULL, textRect.size, NULL);
+//    textSize = CGSizeMake(CGFloat_ceil(textSize.width), CGFloat_ceil(textSize.height)); // Fix for iOS 4, CTFramesetterSuggestFrameSizeWithConstraints sometimes returns fractional sizes
+//
+//    if (textSize.height < bounds.size.height) {
+//        CGFloat yOffset = 0.0f;
+//        switch (self.verticalAlignment) {
+//            case TTTAttributedLabelVerticalAlignmentCenter:
+//                yOffset = CGFloat_floor((bounds.size.height - textSize.height) / 2.0f);
+//                break;
+//            case TTTAttributedLabelVerticalAlignmentBottom:
+//                yOffset = bounds.size.height - textSize.height;
+//                break;
+//            case TTTAttributedLabelVerticalAlignmentTop:
+//            default:
+//                break;
+//        }
+//
+//        textRect.origin.y += yOffset;
+//    }
+//
+//    return textRect;
+//}
+//
+
 //@implementation CustomActionLabel
 //
 //-(instancetype)init {
